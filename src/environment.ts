6c2ea90c9bddaec3ea6dc58bf837db5cddf0f762
bad0dab <broadnicegamerr@gmail.com>
2026-07-19 15:33:11 +0200

    changed environment file, now it should work without .env shown

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  emailJsServiceId: import.meta.env.NG_APP_EMAILJS_SERVICE_ID,
-  emailJsTemplateIdNotify: import.meta.env.NG_APP_EMAILJS_TEMPLATE_ID_NOTIFY,
-  emailJsPublicKey: import.meta.env.NG_APP_EMAILJS_PUBLIC_KEY,
-  googleClientId: import.meta.env.NG_APP_GOOGLE_CLIENT_ID
-};   
+
+  emailJsServiceId: 'service_l8ums9n',
+  emailJsTemplateIdNotify: 'template_g82rvur',
+  emailJsPublicKey: 'DeVcUcw3keRhgO2t3',
+  googleClientId: '55988937584-v6pvh0vaagqbcf1clki9211rs5q85nhf.apps.googleusercontent.com'
+};
